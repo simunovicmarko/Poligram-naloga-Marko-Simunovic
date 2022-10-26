4cfbe6c8be7390bc3927fa69075901e417a4c4db
simunovicmarko <59643387+simunovicmarko@users.noreply.github.com>
2022-10-26 14:24:48 +0000

    All in working order

--- Poligram-naloga-Marko-Simunovic/Program.cs
+++ Poligram-naloga-Marko-Simunovic/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Naloga
 {
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Naloga1 n1 = new Naloga1();
+            n1.execute();
+            // Naloga2 n2 = new Naloga2();
+            // var watch = new System.Diagnostics.Stopwatch();
+            // watch.Start();
+            // Naloga3 n3 = new Naloga3();
+            // Random random = new Random();
+            // for (int i = 0; i < 1000; i++)
+            // {
+            //     // n2.execute();
+            //     n3.execute(random.Next(10, 51));
+            //     // System.Console.WriteLine(n2.analyse());
+            // }
+            // watch.Stop();
+            // System.Console.WriteLine((double)watch.ElapsedMilliseconds / 1000);
+            
+            // n3.execute();
         }
     }
 }
